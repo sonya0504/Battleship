820c7c0c468925086e417e4138cbeb6e0a19cb70
Sonia <sonia.jarczak@serwisprawa.pl>
2019-07-03 09:39:11 +0200

    controller, handleKeyPress

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,4 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    
-    var view = {
-        displayMessage: function(msg) {
-            var messegeArea = document.getElementById('messageArea');
-            messageArea.innerHTML = msg;
-        },
-        displayHit: function(location) {
-            var cell = document.getElementById(location);
-            cell.setAttribute('class', 'hit');
-        },
-        displayMiss: function(location) {
-            var cell = document.getElementById(location);
-            cell.setAttribute('class', 'miss');
-        }
-    }
 
     var model = {
         boardSize: 7,
@@ -21,9 +6,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
         shipLength: 3,
         shipsSunk: 0,
 
-        ships: [{ locations: ["06", "16", "26"], hits: ["", "", ""] },
-            { locations: ["24", "34", "44"], hits: ["", "", ""] },
-            { locations: ["10", "11", "12"], hits: ["", "", ""] }
+        ships: [{ locations: [0, 0, 0], hits: ["", "", ""] },
+            { locations: [0, 0, 0], hits: ["", "", ""] },
+            { locations: [0, 0, 0], hits: ["", "", ""] }
         ],
 
         fire: function(guess) {
@@ -56,7 +41,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
             return true;
         },
 
-        generateShipLocations: function () {
+        generateShipLocations: function() {
             var locations;
             for (var i = 0; i < this.numShips; i++) {
                 do {
@@ -66,7 +51,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
             }
         },
 
-        generateShip: function () {
+        generateShip: function() {
             var direction = Math.floor(Math.random() * 2);
             var row, col;
             if (direction === 1) {
@@ -79,18 +64,18 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
             var newShipLocations = [];
             for (var i = 0; i < this.shipLength; i++) {
-               if (direction === 1) {
-                   newShipLocations.push(row + "" + (col + i));
+                if (direction === 1) {
+                    newShipLocations.push(row + "" + (col + i));
                 } else {
-                   newShipLocations.push((row + i) + "" + col);
-                } 
+                    newShipLocations.push((row + i) + "" + col);
+                }
             }
             return newShipLocations;
         },
 
-        collision: function (lcations) {
+        collision: function(locations) {
             for (var i = 0; i < this.numShips; i++) {
-                var ship = model.ships[i];
+                var ship = this.ships[i];
                 for (var j = 0; j < locations.length; j++) {
                     if (ship.locations.indexOf(locations[j]) >= 0) {
                         return true;
@@ -99,28 +84,22 @@ document.addEventListener("DOMContentLoaded", function(event) {
             }
             return false;
         }
-    }
+    };
 
-    function parseGuess(guess) {
-        var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
-
-        if (guess === null || guess.length !== 2) {
-            alert('Ups, proszę wpisać literę i cyfrę.');
-        } else {
-            firstChar = guess.charAt(0);
-            var row = alphabet.indexOf(firstChar);
-            var column = guess.charAt(1);
-
-            if (isNaN(row) || isNaN(column)) {
-                alert('Ups, to nie są współrzędne!');
-            } else if (row < 0 || row >= model.boardSize || column < 0 || column >= model.boardSize) {
-                alert('Ups, pole poza planszą!');
-            } else {
-                return row + column
-            }
+    var view = {
+        displayMessage: function(msg) {
+            var messgeArea = document.getElementById('messageArea');
+            messageArea.innerHTML = msg;
+        },
+        displayHit: function(location) {
+            var cell = document.getElementById(location);
+            cell.setAttribute('class', 'hit');
+        },
+        displayMiss: function(location) {
+            var cell = document.getElementById(location);
+            cell.setAttribute('class', 'miss');
         }
-        return null
-    }
+    };
 
     var controller = {
         guesses: 0,
@@ -137,14 +116,50 @@ document.addEventListener("DOMContentLoaded", function(event) {
         }
     }
 
-    function init () {
-        var fireButton = document.getElementById('fireButton');
-        fireButton.onclick = handleFireButton;
+    function parseGuess(guess) {
+        var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+
+        if (guess === null || guess.length !== 2) {
+            alert('Ups, proszę wpisać literę i cyfrę.');
+        } else {
+            var firstChar = guess.charAt(0);
+            var row = alphabet.indexOf(firstChar);
+            var column = guess.charAt(1);
+
+            if (isNaN(row) || isNaN(column)) {
+                alert('Ups, to nie są współrzędne!');
+            } else if (row < 0 || row >= model.boardSize || column < 0 || column >= model.boardSize) {
+                alert('Ups, pole poza planszą!');
+            } else {
+                return row + column
+            }
+        }
+        return null
     }
 
-    function handleFireButton () {
+    function handleFireButton() {
         var guessInput = document.getElementById('guessInput');
         var guess = guessInput.value;
+        controller.processGuess(guess);
+    }
+
+    function handleKeyPress(e) {
+        var fireButton = document.getElementById("fireButton");
+
+        e = e || window.event;
+
+        if (e.keyCode === 13) {
+            fireButton.click();
+            return false;
+        }
+    }
+
+    function init() {
+        var fireButton = document.getElementById('fireButton');
+        fireButton.onclick = handleFireButton;
+        var guessInput = document.getElementById('guessInput');
+        guessInput.onkeypress = handleKeyPress;
+        model.generateShipLocations();
     }
 
     window.onload = init;
